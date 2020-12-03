bf294b7baa9fb827b7f4707faaa297430ac5a7d6
Ogilo <alphaogilo@gmail.com>
2020-12-03 11:50:25 +0200

    chore: setup mongodb

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
+const connectDB = require("./database/db");
 const express = require("express");
 
 const app = express();
+
+//connect to database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT,
-    console.log(`Server running in${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
